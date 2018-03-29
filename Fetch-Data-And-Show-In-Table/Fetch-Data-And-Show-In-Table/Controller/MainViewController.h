3a1f0786709b66ab4f5e6cfa652b9f00b2ae7968
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2018-03-30 00:38:21 +0600

    Viewcontroller created with tableview

--- Fetch-Data-And-Show-In-Table/Fetch-Data-And-Show-In-Table/Controller/MainViewController.h
+++ Fetch-Data-And-Show-In-Table/Fetch-Data-And-Show-In-Table/Controller/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  Fetch-Data-And-Show-In-Table
 //
 //  Created by Arafat Mahmood Rahat on 30/3/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
