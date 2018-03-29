aba1f872d59479c44d396e332726786a6be1d725
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2018-03-30 00:43:16 +0600

    Data model class "Info" created - For now it stores 4 info: id, info1, info2, info3

---
+++ Fetch-Data-And-Show-In-Table/Fetch-Data-And-Show-In-Table/Model/Info.h
@@ -0,0 +1,19 @@
+//
+//  Info.h
+//  Fetch-Data-And-Show-In-Table
+//
+//  Created by Arafat Mahmood Rahat on 30/3/18.
+//  Copyright © 2018 Tanjim.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Info : NSObject
+
+@property (nonatomic, assign) NSInteger id;
+@property (nonatomic, strong) NSString *info1;
+@property (nonatomic, strong) NSString *info2;
+@property (nonatomic, strong) NSString *info3;
+
+
+@end
---
+++ Fetch-Data-And-Show-In-Table/Fetch-Data-And-Show-In-Table/Model/Info.m
@@ -0,0 +1,13 @@
+//
+//  Info.m
+//  Fetch-Data-And-Show-In-Table
+//
+//  Created by Arafat Mahmood Rahat on 30/3/18.
+//  Copyright © 2018 Tanjim.org. All rights reserved.
+//
+
+#import "Info.h"
+
+@implementation Info
+
+@end
